4fc07a958a35e5bb79c550c22dfcd481a08bfa84
Kaiyi952 <irishe952@gmail.com>
2025-08-18 18:38:14 -0400

    fix: remove unused import

--- app/components/Form/SubscribeModal.tsx
+++ app/components/Form/SubscribeModal.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import styles from './form.module.scss'
-import { FaRegHandPointLeft } from 'react-icons/fa6';
 interface SubscribeModalProps {
   isOpen: boolean;
   onClose: () => void;
