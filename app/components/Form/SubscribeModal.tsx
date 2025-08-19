e8ecc0268418ec5e84e2ed7750c9b17796cef551
Kaiyi952 <irishe952@gmail.com>
2025-08-19 12:17:45 -0400

    feat: responsive style for subscription model

--- app/components/Form/SubscribeModal.tsx
+++ app/components/Form/SubscribeModal.tsx
@@ -11,18 +11,17 @@ function SubscribeModal({ isOpen, onClose }: SubscribeModalProps) {
   if (!isOpen) return null;
   return (
     <div className={` ${styles.overlay}`} onClick={onClose}>
-      <div className={`w-[60%] h-[200px] bg-white rounded px-10 py-4 `} onClick={(e) => e.stopPropagation()}>
+      <div className={`w-[60%] h-auto bg-white rounded md:px-10 md:py-4 px-4 py-2`} onClick={(e) => e.stopPropagation()}>
         <div className='flex justify-between'>
-          <p className={`${styles.handWritten} `}>Subscribe to my latest blog...</p>
-          <button onClick={onClose} className={`${styles.handWritten} pr-8`}>x</button>
+          <p className={`${styles.handWritten}`}>Subscribe to my latest blog...</p>
         </div>
 
-        <div className='flex w-full pr-20'>
+        <div className='md:flex md:flex-row flex flex-col w-full md:pr-20 pr-0'>
           <input
             type="email"
             placeholder="love@thankyou.com"
             required
-            className={`flex-1 mr-2 focus:outline-none bg-transparent text-[#3c3f75]`}
+            className={`h-[26px] md:h-[38px] md:text-[32px] text-[20px] flex-1 mr-2 self-center focus:outline-none bg-transparent text-[#3c3f75]`}
           />
 
           <button
