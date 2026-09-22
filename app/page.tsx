249bc62f7d88c0b8d9766033e7ac54ee1b2175e3
Kaiyi952 <irishe952@gmail.com>
2026-09-21 21:57:15 -0400

    feat: update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ function Page() {
         <p className={`${styles.subHeader} sm:mt-20 ml-18 sm:ml-20 mt-8`}>about me</p>
         <div className={`${styles.article} ml-12 mt-8 mx-8 sm:ml-8 sm:w-[50vw] sm:mt-20 sm:max-w-[700px]`}>
           Hello world! My name is <Sparkles>Kaiyi</Sparkles>. I&apos;m a passionate <p className="inline-block"><TrueFocus
-            sentence="Software Engineer and AI Developer"
+            sentence="Software&nbsp;Engineer and AI&nbsp;Developer"
             skippedWord="and"
             manualMode={false}
             blurAmount={2}
